650c4814d015a2a57b87b40100feb24b7c6efbdb
osmar <osmarrod18@gmail.com>
2024-08-13 22:46:14 +0100

    lambda function template

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+
+func main() {
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)})
+
+	if err != nil {
+		return
+	}
+	dynaClient = dynamodb.New(awsSession)
+	lambda.Start(handler)
+}
